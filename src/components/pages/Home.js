d9b2ded202f50c1a7293c3826368f642e9e8cd6f
Dreski <dreskirim@gmail.com>
2021-04-06 17:14:01 +0200

    changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,54 +9,50 @@ import SendFleek from '../sendFleek';
 function Home() {
     return (
         <>
-            <body>
+            <div>
                 <header>
                     <nav>
                         <a className='nav' href='#'>Start</a>
                     </nav>
                 </header>
 
-                <main>
-                    <section className='box_1'>
+                <div>
+                    <div className='box_1'>
                         <img className='imagen_prop' src={'/VXN.gif'} alt='VXN Logo'/>
                         <form method='GET' role='search'>
                             <input className='search_bar' type='text' placeholder='Search...'/>
-                            <SendFleek/>
                         </form>
+                        <SendFleek/>
                         <h1 className='font'>Welcome to Voxen Network.</h1>
                         <h2 className='font'>The Republic of 3D Models.</h2>
-                    </section>
+                    </div>
 
-                    <section className='box_2'>
-                        <Link to='/categories'>
-                            <a className='buttom_1'>
-                                Categories
-                            </a>
+                    <div className='box_2'>
+                        <Link to='/categories' className='buttom_1'>
+                            Categories 
                         </Link>
-                        <Link to='/library'>
-                            <a className='buttom_1'>
-                                Library
-                            </a>
+                        <Link to='/library' className='buttom_1'>   
+                            Library
                         </Link>
                         
-                    </section>
+                    </div>
 
                     <h2 className='font'>Featured Volumes</h2>
                     
-                </main>
-                {/* <section class="box_3">
+                </div>
+                {/* <div class="box_3">
                     <a className="buttom_2" href="#">Model 1</a>
                     <a className="buttom_2" href="#">Model 2</a>
                     <a className="buttom_2" href="#">Model 3</a>
                     <a className="buttom_2" href="#">Model 4</a>
                     <a className="buttom_2" href="#">Model 5</a>
                     <a className="buttom_2" href="#">Model 6</a>
-                </section> */}
+                </div> */}
                 <Blocks />
                 <footer>
                     <p>Voxen S.A</p>
                 </footer>
-            </body> 
+            </div> 
         </> 
     );
   }
